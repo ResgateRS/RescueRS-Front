754b0e19e8730d32f8aff4d35a3d344572e66647
Ágata Leuck <agata@leuck.dev>
2024-05-06 22:32:56 -0300

    feat: useApi hook to handle auth token

--- src/screens/Resgates/index.tsx
+++ src/screens/Resgates/index.tsx
@@ -1,138 +1,160 @@
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import Layout from "../../components/Layout";
 import { useInfiniteQuery } from "@tanstack/react-query";
-import { Alert, ListGroup, Spinner, Tab, Tabs } from "react-bootstrap";
+import { Alert, ListGroup, Tab, Tabs } from "react-bootstrap";
 import { useAuth } from "../../context/AuthContext";
 import React from "react";
 import RestageItem from "../../components/RestageItem";
 import { APIResponseListPengingRescues } from "../../config/define";
 import Header from "../../components/Header";
 import InfiniteScroll from "../../components/InfiniteScroll";
+import { useApi } from "../../hooks/api";
 
 export default function Resgates() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const { latitude, longitude, token } = useAuth();
+  const { get } = useApi();
 
-  async function fetchDataPending(page: any) {
-    let resp = await  fetch(`${import.meta.env.VITE_API_URL}/Rescue/ListPendingRescues?latitude=${latitude}&longitude=${longitude}`,
+  async function fetchDataPending(page?: number) {
+    return await get<APIResponseListPengingRescues>(
+      `${import.meta.env.VITE_API_URL}/Rescue/ListPendingRescues`,
       {
-        headers: page ? {
-					Authorization: `Bearer ${token}`,
-					"X-Cursor": page,
-					"X-PageSize": import.meta.env.VITE_PAGE_SIZE
-				}:{
-					Authorization: `Bearer ${token}`,
-					"X-PageSize": import.meta.env.VITE_PAGE_SIZE
-				},
+        search:
+          typeof latitude === "number" && typeof longitude === "number"
+            ? new URLSearchParams({
+                latitude: latitude.toString(),
+                longitude: longitude.toString(),
+              })
+            : undefined,
+        headers: page
+          ? {
+              "X-Cursor": page.toString(),
+              "X-PageSize": import.meta.env.VITE_PAGE_SIZE,
+            }
+          : {
+              "X-PageSize": import.meta.env.VITE_PAGE_SIZE,
+            },
       }
     );
-		return await resp.json() as APIResponseListPengingRescues;
   }
 
-  async function fetchDataPendingNextPage(){
+  async function fetchDataPendingNextPage() {
     queryPending.fetchNextPage();
   }
 
-  async function fetchDataCompleted(page: any) {
-    let resp = await  fetch(`${import.meta.env.VITE_API_URL}/Rescue/ListCompletedRescues?latitude=${latitude}&longitude=${longitude}`,
+  async function fetchDataCompleted(page?: number) {
+    return await get<APIResponseListPengingRescues>(
+      `${
+        import.meta.env.VITE_API_URL
+      }/Rescue/ListCompletedRescues?latitude=${latitude}&longitude=${longitude}`,
       {
-        headers: page ? {
-					Authorization: `Bearer ${token}`,
-					"X-Cursor": page,
-					"X-PageSize": import.meta.env.VITE_PAGE_SIZE
-				}:{
-					Authorization: `Bearer ${token}`,
-					"X-PageSize": import.meta.env.VITE_PAGE_SIZE
-				},
+        headers:
+          typeof page === "number"
+            ? {
+                "X-Cursor": page.toString(),
+                "X-PageSize": import.meta.env.VITE_PAGE_SIZE,
+              }
+            : {
+                "X-PageSize": import.meta.env.VITE_PAGE_SIZE,
+              },
       }
     );
-		return await resp.json() as APIResponseListPengingRescues;
   }
 
-  async function fetchDataCompletedNextPage(){
+  async function fetchDataCompletedNextPage() {
     queryCompleted.fetchNextPage();
   }
 
   const queryPending = useInfiniteQuery<APIResponseListPengingRescues>({
-    queryKey: ["ListPengingRescues"],
-    queryFn: ({ pageParam }) => fetchDataPending(pageParam),
+    queryKey: ["ListPengingRescues", token],
+    queryFn: ({ pageParam }) => fetchDataPending(pageParam as number),
     initialPageParam: undefined,
-    getNextPageParam: (lastPage) => lastPage.Data.length>0 ? lastPage.Data[lastPage.Data.length-1].rescueId : null,
+    getNextPageParam: (lastPage) =>
+      lastPage.Data.length > 0
+        ? lastPage.Data[lastPage.Data.length - 1].rescueId
+        : null,
   });
   const queryCompleted = useInfiniteQuery<APIResponseListPengingRescues>({
-    queryKey: ["ListCompletedRescues"],
-    queryFn: ({ pageParam }) => fetchDataCompleted(pageParam),
+    queryKey: ["ListCompletedRescues", token],
+    queryFn: ({ pageParam }) => fetchDataCompleted(pageParam as number),
     initialPageParam: undefined,
-    getNextPageParam: (lastPage) => lastPage.Data.length>0 ? lastPage.Data[lastPage.Data.length-1].rescueId : null,
+    getNextPageParam: (lastPage) =>
+      lastPage.Data.length > 0
+        ? lastPage.Data[lastPage.Data.length - 1].rescueId
+        : null,
   });
 
   queryPending.isSuccess && console.log(queryPending.data.pages[0]);
 
   return (
     <Layout>
-      <Header/>
-			<h4 className="mb-4">Resgates</h4>
+      <Header />
+      <h4 className="mb-4">Resgates</h4>
 
       <Tabs defaultActiveKey="pending" className="w-100" fill>
         <Tab eventKey="pending" title="Pendentes">
-            {queryPending.isFetched && queryPending.data?.pages[0].Data?.length===0 && (
+          {queryPending.isFetched &&
+            queryPending.data?.pages[0].Data?.length === 0 && (
               <Alert variant="light">Nenhum registro encontrado</Alert>
             )}
-            <ListGroup className="w-100">
-              {queryPending.data?.pages.map((page, key)=>{
-                return (
-                  <React.Fragment key={key}>
-                    {page.Data.map((item, itemKey)=>{
-                      return (
-                        <RestageItem
-                          key={itemKey}
-                          animalsNumber={item.animalsNumber}
-                          childrenNumber={item.childrenNumber}
-                          elderlyNumber={item.elderlyNumber}
-                          totalPeopleNumber={item.totalPeopleNumber}
-                        />
-                      )
-                    })}
-                  </React.Fragment>
-                )
-              })}
-            </ListGroup>
+          <ListGroup className="w-100">
+            {queryPending.data?.pages.map((page, key) => {
+              return (
+                <React.Fragment key={key}>
+                  {page.Data.map((item, itemKey) => {
+                    return (
+                      <RestageItem
+                        key={itemKey}
+                        animalsNumber={item.animalsNumber}
+                        childrenNumber={item.childrenNumber}
+                        elderlyNumber={item.elderlyNumber}
+                        totalPeopleNumber={item.totalPeopleNumber}
+                      />
+                    );
+                  })}
+                </React.Fragment>
+              );
+            })}
+          </ListGroup>
 
-            <InfiniteScroll
-              more={queryPending.hasNextPage}
-              load={fetchDataPendingNextPage}
-              loading={queryPending.isFetching || queryPending.isFetchingNextPage}
-            />
+          <InfiniteScroll
+            more={queryPending.hasNextPage}
+            load={fetchDataPendingNextPage}
+            loading={queryPending.isFetching || queryPending.isFetchingNextPage}
+          />
         </Tab>
         <Tab eventKey="completed" title="Concluídos">
-            {queryCompleted.isFetched && queryCompleted.data?.pages[0].Data?.length===0 && (
+          {queryCompleted.isFetched &&
+            queryCompleted.data?.pages[0].Data?.length === 0 && (
               <Alert variant="light">Nenhum registro encontrado</Alert>
             )}
-            <ListGroup className="w-100">
-              {queryCompleted.data?.pages.map((page, key)=>{
-                return (
-                  <React.Fragment key={key}>
-                    {page.Data.map((item, itemKey)=>{
-                      return (
-                        <RestageItem
-                          key={itemKey}
-                          animalsNumber={item.animalsNumber}
-                          childrenNumber={item.childrenNumber}
-                          elderlyNumber={item.elderlyNumber}
-                          totalPeopleNumber={item.totalPeopleNumber}
-                        />
-                      )
-                    })}
-                  </React.Fragment>
-                )
-              })}
-            </ListGroup>
+          <ListGroup className="w-100">
+            {queryCompleted.data?.pages.map((page, key) => {
+              return (
+                <React.Fragment key={key}>
+                  {page.Data.map((item, itemKey) => {
+                    return (
+                      <RestageItem
+                        key={itemKey}
+                        animalsNumber={item.animalsNumber}
+                        childrenNumber={item.childrenNumber}
+                        elderlyNumber={item.elderlyNumber}
+                        totalPeopleNumber={item.totalPeopleNumber}
+                      />
+                    );
+                  })}
+                </React.Fragment>
+              );
+            })}
+          </ListGroup>
 
-            <InfiniteScroll
-              more={queryCompleted.hasNextPage}
-              load={fetchDataCompletedNextPage}
-              loading={queryCompleted.isFetching || queryCompleted.isFetchingNextPage}
-            />
+          <InfiniteScroll
+            more={queryCompleted.hasNextPage}
+            load={fetchDataCompletedNextPage}
+            loading={
+              queryCompleted.isFetching || queryCompleted.isFetchingNextPage
+            }
+          />
         </Tab>
       </Tabs>
     </Layout>
